885afce26c7a3556422af19bbebafc05b492e081
Geovane Santiago <matheushhus@gmail.com>
2024-08-21 14:21:02 -0300

    aula 04 print

---
+++ aula04/aula04.py
@@ -0,0 +1,54 @@
+
+
+
+
+
+# numero1 = int(input('digite o primeiro numero \n'))
+# numero2 = int(input('digite o segundo numero \n'))15
+
+# print(numero1 + numero2)
+# print(type(numero1))
+# print(type(numero2))
+
+# nome = 'Goevane'
+# idade = 18
+# altura = 1.73
+# vivo = False
+
+
+# int()
+# float()
+# str()
+# bool()
+
+#  float(idade)
+
+# print(int(vivo))
+
+# print(type(nome))
+# print(type(str(idade)))
+# print(type(altura))
+# print(type(vivo))
+
+
+
+numero1 = int(input('digite o primeiro numero \n'))
+numero2 = int(input('digite o segundo numero \n'))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisão = numero1 / numero2
+
+
+print('a soma é: ', soma)
+print('a subtracao é: '+ str(subtracao))
+print('a multiplicacao é:{} e divisao foi {} '.format(multiplicacao, divisão))
+print(f'a {multiplicacao} é: e {divisão} foi')
+
+
+
+# print(soma)
+# print(subtracao)
+# print(multiplicacao)
+# print(divisão)
