9751c13697d5357184bc9edcb053946c3a103854
Geovane Santiago <matheushhus@gmail.com>
2024-08-21 16:10:03 -0300

    estrutura de repetição

---
+++ aula06/aula06.py
@@ -0,0 +1,91 @@
+#candidato = int(input('Informe o numero do candidato \n'))
+
+
+
+
+#numero = 10
+#print(numero)
+
+#numero = numero + 10
+#print(numero)
+#numero += 10
+
+#numero = numero - 10
+#print(numero)
+#numero -= 10
+
+#numero = numero * 10
+#print(numero)
+#numero *= 10
+
+#numero = numero / 10
+#print(numero)
+#numero /= 10
+
+#numero = int(input('informe o numero\n'))
+
+#if numero % 2 == 0:
+     # print('o numero é par')
+#else:
+   #   print('o numero é inpar')      
+
+
+
+     # laços de repetição
+
+#for i in range(5):
+    #print("geovane")
+
+#nomes = ['geovane', 'geovane', 'geovane']
+
+#print(nomes)
+
+#for nome in nomes:
+   #   print(nome)
+
+# frutas = ['banana', 'maçã', 'morango', 'uva']
+# print(frutas[2])
+
+# #for fruta in frutas:
+#  #     print(fruta)
+
+# for indice, fruta in enumerate(frutas):
+#       print(f'suas frutas são: {indice} - {frutas}')
+# nomes = []
+# for i in range(5):
+#       nome = input('informeseu nome: \n')
+#       nomes.append(nome)
+
+# for nome in nomes:
+#       print(nome)      
+
+
+
+# nome = 'geovane'
+
+# for i in nome:
+#       print(i)
+
+# while em português enquanto
+
+# nomero = nome 
+
+
+# while numero != 0:
+#       numero = int(input('informe o numero'))
+
+# contador = 1
+
+# numero = int(input('informe o numero: '))
+
+# while contador < 10:
+#       print(numero * 2)
+#       contador +=1
+
+
+numero = 10
+while True:
+      numero *= 10
+      
+      if numero > 1000000:
+            break 
